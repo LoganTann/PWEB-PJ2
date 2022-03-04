e77a41310aa040ffe720682822a0a3bca2524d82
LoganTann <logon313@hotmail.fr>
2022-03-05 00:46:03 +0100

    feat: Pistes chargées dynamiquement, légende

--- src/components/mapComponents/PistesCyclables.jsx
+++ src/components/mapComponents/PistesCyclables.jsx
@@ -1,42 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { useMapEvents, LayerGroup, Polyline } from "react-leaflet";
-import { getPistesCyclables } from "../../utils/parisOpenData";
+import { getPistesCyclables, getColor } from "../../utils/parisOpenData";
+import Legende from "./Legende";
 
 /**
  * LocationMarker component : Marqueur automatique affichant la position actuelle de l'utilisateur.
  * Pour le mettre à jour, utiliser l'event LocateBtnClicked via EventEmitter.
  */
 export default function PistesCyclables() {
-  const [positions, setPositions] = useState(null);
+    const [positions, setPositions] = useState(null);
 
-  const map = useMapEvents({
-    zoomend(e) {
-      console.log("Vous avez changé le niveau de zoom", e);
-    },
-    resize(e) {
-      console.log("Vous avez redimensionné la carte", e);
-    },
-    moveend(e) {
-      console.log("Vous avez déplacé la carte", e);
-    },
-  });
+    function onMapChange(map) {
+        const { lat, lng } = map.getCenter();
+        const { _northEast, _southWest } = map.getBounds();
+        const location = `${map.getZoom()},${lat},${lng}`;
+        const bbox = `${_southWest.lat},${_southWest.lng},${_northEast.lat},${_northEast.lng}`;
+        getPistesCyclables(location, bbox).then((pistes) => {
+            setPositions(pistes);
+        });
+    }
 
-  useEffect(() => {
-    getPistesCyclables().then((data) => {
-      setPositions(data);
+    const map = useMapEvents({
+        moveend() {
+            onMapChange(map);
+        },
+        load() {
+            onMapChange(map);
+        },
     });
-    // mettre un tableau vide pour executer une seule fois
-  }, []);
-  
-  const limeOptions = { color: 'blue', weight: 2, opacity: 0.5 };
 
-  return positions === null ? null : (
-    <LayerGroup>
-      {
-        positions.map((piste) => (
-          <Polyline pathOptions={limeOptions} positions={piste.coordinates} key={piste.id}/>
-        ))
-      }
-    </LayerGroup>
-  );
+    function pathStyle(layer) {
+        const zoom = map.getZoom();
+        return {
+            color: getColor.of(layer),
+            weight: zoom > 12 && zoom < 15 ? 2 : 5,
+            opacity: 0.5,
+        };
+    }
+
+    return positions === null ? null : (
+        <LayerGroup>
+            {positions.map((piste) => (
+                <Polyline
+                    pathOptions={pathStyle(piste.layer)}
+                    positions={piste.coordinates}
+                    key={piste.id}
+                />
+            ))}
+            <Legende map={map} />
+        </LayerGroup>
+    );
 }
